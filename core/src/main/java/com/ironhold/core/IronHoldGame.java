623eeeaf72e5dcaae043c6a07191a72213e55960
Aldiyar <aldiyar.zhartybek@narxoz.kz>
2026-04-18 00:14:34 +0500

    route screens through GameFacade

--- core/src/main/java/com/ironhold/core/IronHoldGame.java
+++ core/src/main/java/com/ironhold/core/IronHoldGame.java
@@ -5,6 +5,7 @@ import com.ironhold.assets.AssetService;
 import com.ironhold.events.GameStartedEvent;
 import com.ironhold.events.SimpleEventBus;
 import com.ironhold.game.GameContext;
+import com.ironhold.game.GameFacade;
 import com.ironhold.game.screen.ScreenId;
 import com.ironhold.game.screen.ScreenManager;
 
@@ -16,27 +17,33 @@ public class IronHoldGame extends Game {
     private AssetService assets;
     private ScreenManager screens;
     private GameContext context;
+    private GameFacade facade;
 
     @Override
     public void create() {
         context = new GameContext(new SimpleEventBus());
         assets = new AssetService();
         screens = new ScreenManager(this);
-        screens.register(ScreenId.LOADING, () -> new LoadingScreen(screens, assets));
-        screens.register(ScreenId.MENU, () -> new MenuScreen(screens, assets));
-        screens.register(ScreenId.GAME, () -> new GameScreen(assets));
-        context.getEventBus().publish(new GameStartedEvent());
+        facade = new GameFacade(context, assets, screens);
+        screens.register(ScreenId.LOADING, () -> new LoadingScreen(facade));
+        screens.register(ScreenId.MENU, () -> new MenuScreen(facade));
+        screens.register(ScreenId.GAME, () -> new GameScreen(facade));
+        facade.getEventBus().publish(new GameStartedEvent());
         screens.goTo(ScreenId.LOADING);
     }
 
     @Override
     public void dispose() {
         super.dispose();
-        context.getEventBus().clear();
+        facade.getEventBus().clear();
         assets.dispose();
     }
 
     public GameContext getContext() {
         return context;
+    }
+
+    public GameFacade getFacade() {
+        return facade;
     }
 }
