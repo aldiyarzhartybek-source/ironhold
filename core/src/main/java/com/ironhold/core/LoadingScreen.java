9514560714a4fbbd331fde76466596f9e6a185a9
Aldiyar <aldiyar.zhartybek@narxoz.kz>
2026-05-18 23:13:08 +0500

    Apply GameTheme to Scene2D skin and non-gameplay screens

--- core/src/main/java/com/ironhold/core/LoadingScreen.java
+++ core/src/main/java/com/ironhold/core/LoadingScreen.java
@@ -2,8 +2,8 @@ package com.ironhold.core;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.graphics.GL20;
 import com.ironhold.game.GameFacade;
+import com.ironhold.ui.GameTheme;
 import com.ironhold.game.screen.ScreenId;
 
 import java.util.Objects;
@@ -23,8 +23,7 @@ public final class LoadingScreen extends ScreenAdapter {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0.02f, 0.02f, 0.05f, 1f);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        GameTheme.clearBackground();
 
         if (game.getAssets().update()) {
             game.getScreens().goTo(ScreenId.MENU);
